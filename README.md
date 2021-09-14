992da27afd640d4bec9874a4e832299e86b704e8
VanessaLops <vanessashirlenee_estrela@hotmail.com>
2021-09-13 23:36:59 -0300

    utilizando os métodos HTTP

---
+++ README.md
@@ -0,0 +1,8 @@
+Anotações Inportantes 
+
+Métodos HTTP 
+[GET]   =>  Buscar uma informação dentro do servidor.
+[POST]  =>  Inserir uma informação no servidor.
+[PUT]   =>  Alterar uma informação no servidor.
+[PATCH] =>  Alteração de Avatar/alterar email [Fazer alteração especifica].
+[DELETE]=> Deletar uma informação no servidor.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 //método sende envia mensagem para quem faz a requisição.
-app.get("/",(request,response)=>{
-return response.json({message:"Hello Word Teste Nodemon!"})
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
 
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3"]);
+});
 
-})
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3"]);
+});
 //Iniciando a porta
-app.listen(2005)
+app.listen(2005);
