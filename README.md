a0fc594b45b1694a598d3c2573598b906537afd2
VanessaLops <vanessashirlenee_estrela@hotmail.com>
2021-09-16 04:33:55 -0300

    finalizado os fundamentos de node.js

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 Anotações Inportantes 
 
+Iniciando o terminal com nodemon
+
+comando: yarn dev
 Métodos HTTP 
 [GET]   =>  Buscar uma informação dentro do servidor.
 [POST]  =>  Inserir uma informação no servidor.
 [PUT]   =>  Alterar uma informação no servidor.
 [PATCH] =>  Alteração de Avatar/alterar email [Fazer alteração especifica].
 [DELETE]=> Deletar uma informação no servidor.
+
+Tipos de Parametros
+[Route Params] => Serve para Indentificação de recurso editar ou deletar um recurso e buscar recurso
+[Query Params] =: Serve para Paginação/ Filtro de busca
+[Body Params] =: Serve para Inserção ou alteração de recurso ex: cadastro padrão JSON
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 
 const app = express();
-
+//para utilizar o json na aplicação
+app.use(express.json())
 //método sende envia mensagem para quem faz a requisição.
 app.get("/courses", (request, response) => {
-  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+  const query = request.query;
+  //return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+  return response.json(query);
 });
 
 app.post("/courses", (request, response) => {
-  return response.json(["Curso 1", "Curso 2", "Curso 3","Curso 4"]);
+  const body = request.body;
+  console.log(body)
+ // return response.json(["Curso 1", "Curso 2", "Curso 3","Curso 4"]);
+ return response.json(body);
 });
 app.put("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 2", "Curso 3"]);
+  const {id} = request.params;
+ // console.log(id)
+  //return response.json(["Curso 6", "Curso 2", "Curso 3"]);
+  return response.json(id);
 });
 
 app.patch("/courses/:id", (request, response) => {
